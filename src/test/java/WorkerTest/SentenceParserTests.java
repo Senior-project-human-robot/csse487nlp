644679369a04addfd7796253a628da4a1b173b15
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-09-13 18:32:24 -0400

    Add unit tests

---
+++ src/test/java/WorkerTest/SentenceParserTests.java
@@ -0,0 +1,19 @@
+package WorkerTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SentenceParserTests {
+
+    @Test
+    public void TestSetup(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void TestParse(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+}
