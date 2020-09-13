08b9b4d33fcbb18e0811a0c586f54865027b8626
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-09-13 18:32:24 -0400

    Add unit tests

---
+++ src/test/java/ModelTest/SentenceParseResultTests.java
@@ -0,0 +1,31 @@
+package ModelTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SentenceParseResultTests {
+
+    @Test
+    public void TestGetCommand(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void TestGetPrepositionMap(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void TestGetObjectMap(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void TestGetModsForObjects(){
+        // TODO
+        Assert.assertTrue(false);
+    }
+}
